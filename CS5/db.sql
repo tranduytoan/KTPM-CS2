eb020a634c68c79d6c58e24244c959e7bf7fc08d
Cuong Tran <iammaytinhdibo@gmail.com>
2025-03-17 00:06:05 +0700

    CS5: add commit to schema

--- CS5/db.sql
+++ CS5/db.sql
@@ -1,7 +1,8 @@
 CREATE TABLE IF NOT EXISTS `release` (
-	`name` text NOT NULL,
+	`id` int NOT NULL UNIQUE,
 	`content` text NOT NULL,
-	`repoID` int NOT NULL
+	`repoID` int NOT NULL,
+	PRIMARY KEY (`id`)
 );
 
 CREATE TABLE IF NOT EXISTS `repo` (
@@ -11,5 +12,12 @@ CREATE TABLE IF NOT EXISTS `repo` (
 	PRIMARY KEY (`id`)
 );
 
+CREATE TABLE IF NOT EXISTS `commit` (
+	`hash` text NOT NULL,
+	`message` text NOT NULL,
+	`releaseID` int NOT NULL
+);
+
 
 ALTER TABLE `repo` ADD CONSTRAINT `repo_fk0` FOREIGN KEY (`id`) REFERENCES `release`(`repoID`);
+ALTER TABLE `commit` ADD CONSTRAINT `commit_fk2` FOREIGN KEY (`releaseID`) REFERENCES `release`(`id`);
